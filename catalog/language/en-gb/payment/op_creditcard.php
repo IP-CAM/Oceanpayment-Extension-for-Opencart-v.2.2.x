e990296a38020cb293787604f2e332c7ad2f6226
susan <susanwan@oceanpayment.com.cn>
2022-04-01 10:42:05 +0800

    信用卡带3D功能(站内&站外)

---
+++ catalog/language/en-gb/payment/op_creditcard.php
@@ -0,0 +1,42 @@
+<?php
+// Heading
+$_['heading_title']     = 'Thank you for shopping with %s .... ';
+
+// Text
+$_['text_title']        = 'Credit Card Payment <br/><img src="catalog/view/theme/default/image/op_creditcard.png" alt="Credit Card">';
+$_['text_response']     = 'Response from Credit Card:';
+$_['text_success']      = 'Your payment was successfully received.';
+$_['text_success_wait'] = 'whilst we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s" target="_top" >here</a>.';
+$_['text_failure']      = 'Your payment has been cancelled!';
+$_['text_failure_wait'] = '<br>If you are not automatically re-directed, please click <a href="%s" target="_top" >here</a>.';										  
+
+
+
+//钱海信用卡响应代码解决方案,更新日期2015-04-12
+$_['text_actionMsg_1'] = '1. Try to Pay again, your card issuer may accept your payment.<br>2. Call the 800 number on the back of the card, ask your card issuer accept your payment, then pay again.<br>3. Change another card to pay.';
+$_['text_actionMsg_2'] = 'Contact the merchant website to confirm the transactions.';
+$_['text_actionMsg_3'] = 'Contact the merchant website to confirm the transaction amount.';
+$_['text_actionMsg_4'] = '1. Please confirm the card number is right.<br>2. Change another card to pay.';
+$_['text_actionMsg_5'] = '1. Please confirm the card has enough money or use another card.<br>2. Change another card to pay.';
+$_['text_actionMsg_6'] = 'Please confirm the card period of use.';
+$_['text_actionMsg_7'] = 'Please input the right PIN.';
+$_['text_actionMsg_8'] = 'Please input the right 3-digital CVV2/CSC.';
+$_['text_actionMsg_9'] = '1. Please contact your card issuer to fix the problem.<br>2. Change another card to pay.';
+$_['text_actionMsg_10'] = '1. Please call your card issuer to confirm your account is valid.<br>2. Change another card to pay.';
+$_['text_actionMsg_11'] = '1. Contact the merchant website to confirm the payment result.<br>2. Try to pay again.';
+$_['text_actionMsg_12'] = 'Please contact the merchant website.';
+$_['text_actionMsg_13'] = 'Change another card to pay.';
+$_['text_actionMsg_14'] = 'Please contact the payment service company.';
+$_['text_actionMsg_15'] = 'Try to pay again.';
+$_['text_actionMsg_16'] = 'Cannot Start 3D Authorized Service.';
+$_['text_actionMsg_17'] = 'Input right 3D secure code or change another card to pay.';
+$_['text_actionMsg_18'] = 'Fail to process the 3D transaction techonolly.';
+$_['text_actionMsg_19'] = '1. Try again, finish the transaction correctly.<br>2. Contact website with screenshot if it has error.';
+$_['text_actionMsg_20'] = 'Pay again or change another bank account to pay.';
+$_['text_actionMsg_21'] = 'Try again later or to  choose different payment scheme or bank account.';
+$_['text_actionMsg_22'] = 'Try to pay again and finish the payment.';
+
+
+
+
+?>
